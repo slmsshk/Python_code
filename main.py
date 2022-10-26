a4c23d82d9023de78e85b119dbf7c78c6dd4537e
Salem Shaikh <52129128+slmsshk@users.noreply.github.com>
2022-10-26 19:41:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import numpy as np
-
+import time
 import streamlit as st
+st.set_page_config(page_title="Forecasting Models", page_icon="📈")
+
+for i in range(5):
+    st.balloons()
+    time.sleep(.5)
+
+
 
 st.write(f'Streamlit version {st.__version__}')
 st.write(f'Numpy version {np.__version__}_')
+
+# st.sidebar()
