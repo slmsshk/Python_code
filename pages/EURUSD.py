5574d1c33b9a33e513b3fe285109041a29611e24
Salem Shaikh <52129128+slmsshk@users.noreply.github.com>
2022-10-28 19:45:18 +0530

    Update
    Icon and page config update

--- pages/EURUSD.py
+++ pages/EURUSD.py
@@ -3,6 +3,12 @@ import yfinance as yf
 import plotly.express as px
 import plotly.graph_objects as go
 
+st.set_page_config(page_title="Eur/USD", page_icon='pages\eurusd.png')
+
+# def icon(url):
+    
+
+
 st.title('EUR/USD;')
 eur=yf.Ticker("EURUSD=X")
 hist = eur.history(interval='1h',period='s')
@@ -17,19 +23,15 @@ st.write(fig)
  
 fig=px.line(data_frame=hist,y='Close')
 
-# fig = px.line(hist,y='Close').Figure(data=[go.Candlestick(x=hist.index,
-#                 open=hist['Open'],
-#                 high=hist['High'],
-#                 low=hist['Low'],
-#                 close=hist['Close'])])
-# fig.update_layout(xaxis_rangeslider_visible=False)
-
 st.write(fig)
 
+st.write(f"<p style='font-size:15px;'>Preparing Data</p>")
 X=[]
 Y=[]
 end=3
 x=hist.Close.dropna().values
+
+
 for i in range(len(x)):
     end+=1
     if end>=len(x)-1:break
@@ -38,3 +40,5 @@ for i in range(len(x)):
 
 from keras.models import Sequential
 from keras.layers import LSTM,Dense
+
+
