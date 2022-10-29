e8767588fa54514cfa86c399712d5ba4e2191ca6
Salem Shaikh <52129128+slmsshk@users.noreply.github.com>
2022-10-29 16:16:09 +0530

    Update training.py

--- training.py
+++ training.py
@@ -36,7 +36,7 @@ def Evaluation(X_test,y_test):
     nn=keras.models.load_model('Trained')
     pred=nn.predict(X_test)
     fig,ax=plt.subplots()
-    ax.plot(pred,label='prediction',color='orange')
+    ax.plot(pred[:,1],label='prediction',color='orange')
     ax.plot(y_test,label='Actual',color='red')
     # plt.xticks(hist.index[:-100])
     ax.legend()
